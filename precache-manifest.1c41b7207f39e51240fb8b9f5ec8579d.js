f9b4932bb1b2bbfd99a3b8ababd0e454f6b4b0f9
Walter Wan <walter@trayt.io>
2020-08-11 16:02:51 -0700

    Updates

---
+++ precache-manifest.1c41b7207f39e51240fb8b9f5ec8579d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb711a289b8b43f967716b4fda01837",
+    "url": "/time_translator/index.html"
+  },
+  {
+    "revision": "55633fdcc5e4b93b511e",
+    "url": "/time_translator/static/css/main.4b0e4e1a.chunk.css"
+  },
+  {
+    "revision": "6af0efaf0c9c1ebb40ad",
+    "url": "/time_translator/static/js/2.948c66d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/time_translator/static/js/2.948c66d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55633fdcc5e4b93b511e",
+    "url": "/time_translator/static/js/main.b28b0429.chunk.js"
+  },
+  {
+    "revision": "8250f9e16188570d8583",
+    "url": "/time_translator/static/js/runtime-main.953a3e09.js"
+  }
+]);
